6b1e784e7c2d10bacee3870bcbffc7cd419fceef
Nazim <nazimsiddiqui1993@gmail.com>
2018-04-09 12:59:08 +0530

    Initial Commit

---
+++ BricksGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BricksGame
+//
+//  Created by Nikunj Agrawal on 09/04/18.
+//  Copyright © 2018 Nikunj Agrawal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
